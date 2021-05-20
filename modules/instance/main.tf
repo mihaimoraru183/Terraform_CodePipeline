b940efdd56031cd49f089d3f239b874f483090fe
Mihai Moraru <for.mihaimoraru@gmail.com>
2021-05-20 10:51:45 +0300

    Attached role to instance.

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "codedeploy_instance" {
   ami                   = "ami-0aaaf7510b24fd3d6"
   instance_type         = var.instance_type
   key_name              = "mihai-aws"                       
-  #iam_instance_profile  = aws_iam_instance_profile.instance.name
+  iam_instance_profile  = aws_iam_instance_profile.instance_profile.name
 
   tags = {
     Name = "CodeDeploy_Instance"
@@ -20,17 +20,24 @@ resource "aws_instance" "codedeploy_instance" {
 resource "aws_iam_role" "instance_role" {
   name = "instance_role_terraform"
   description = "Instance Role by Terraform"
-  assume_role_policy = file("assume-role-policy.json")
+  assume_role_policy = file("${path.module}/iam/assume-role-policy.json")
 }
 
 #create a policy for EC2
 resource "aws_iam_policy" "instance_policy" {
   name = "instance_policy_terraform"
   description = "Instance Policy by Terraform"
-  policy = file("policy.json")
+  policy = file("${path.module}/iam/policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "instance_attach" {
+ # name       = "instance_attach_terraform"
   role       = aws_iam_role.instance_role.name
   policy_arn = aws_iam_policy.instance_policy.arn
 }
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "instance_profile"
+  role = aws_iam_role.instance_role.name
+}
+
